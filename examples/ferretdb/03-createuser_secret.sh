20004681610e5803634eccdf32bcba830728de0b
Álvaro Hernández <aht@ongres.com>
2023-05-05 12:39:49 +0200

    FerretDB example: fix small bug in password generation
    Spooted by @teoincontatto, thanks!

--- examples/ferretdb/03-createuser_secret.sh
+++ examples/ferretdb/03-createuser_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PASSWORD=`dd if=/dev/urandom bs=1 count=8 status=none |base64`
+PASSWORD="$(dd if=/dev/urandom bs=1 count=8 status=none | base64 | tr / 0)"
 
 kubectl -n ferretdb create secret generic createuser \
   --from-literal=sql="create user ferretdb with password '"${PASSWORD}"'"
